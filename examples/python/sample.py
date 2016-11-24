927ab279ac02b15fd16a19001a5a436f9e0ade10
xxj <xxj@xxj-no-MacBook-Pro-15-Retina.local>
2016-11-24 20:24:01 +0900

    fix python sample segv

--- examples/python/sample.py
+++ examples/python/sample.py
@@ -1,10 +1,13 @@
+#please make shared library first
 from ctypes import *
-lib = cdll.LoadLibrary("./libmomentum_url_normalizer.a")
+lib = cdll.LoadLibrary("../../libmomentum_url_normalizer.a")
+
 ptr = c_char_p("")
 lib.first_normalize_url("http://example.com/path/", pointer(ptr))
-print ptr
-lib.free_normalize_url(pointer(ptr))
+print ptr.value
+lib.free_normalize_url(ptr)
 
-#ptr = lib.second_normalize_url(c_char_p("http://example.com/path/"))
-#print c_char_p(ptr).value
-#lib.free_normalize_url(ptr)
+ptr = c_char_p("")
+lib.second_normalize_url("http://example.com/path/", pointer(ptr))
+print ptr.value
+lib.free_normalize_url(ptr)

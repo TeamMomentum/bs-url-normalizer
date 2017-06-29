ae1bf9b8b4897ad0dc50939bec5a705b940659e6
xxj <fcsxxj@gmail.com>
2017-06-29 20:06:06 +0900

    separate funcs to files

--- lib/urls/normalizer_test.go
+++ lib/urls/normalizer_test.go
@@ -1,3 +1,8 @@
+// Copyright 2017 Momentum K.K. All rights reserved.
+// This source code or any portion thereof must not be
+// reproduced or used in any manner whatsoever.
+
+// Package urls normalizes URLs and implements its helpers
 package urls
 
 import (
@@ -51,40 +56,6 @@ func TestSplitNDomainPath(t *testing.T) {
 	ul := mustURL("http://example.com/a/b/c/")
 	if splitNDomainPath(ul, 2) != "example.com/a/b" {
 		t.Errorf("%v != %v", splitNDomainPath(ul, 2), "example.com/a/b")
-	}
-}
-
-/*
-パス階層レベルでの正規化のテスト
-*/
-func TestNormalizePathMap(t *testing.T) {
-	var (
-		ul         *url.URL
-		normalized bool
-	)
-
-	ul = mustURL("http://bannch.com/bs/bbs/798793/sub/index.html?test=123")
-	normalized = normalizePath(ul)
-	if !normalized {
-		t.Errorf("%v should be normalized", ul.String())
-		return
-	}
-
-	if ul.String() != "http://bannch.com/bs/bbs/798793" {
-		t.Errorf("%v should be %v", ul.String(), "http://bannch.com/bs/bbs/798793")
-		return
-	}
-
-	ul = mustURL("http://bbs.mottoki.com/index?bbs=kinyuu&thread=&page=2")
-	normalized = normalizePath(ul)
-	if !normalized {
-		t.Errorf("%v should be normalized", ul.String())
-		return
-	}
-
-	if ul.String() != "http://bbs.mottoki.com/index?bbs=kinyuu" {
-		t.Errorf("%v should be %v", ul.String(), "http://bbs.mottoki.com/index?bbs=kinyuu")
-		return
 	}
 }
 

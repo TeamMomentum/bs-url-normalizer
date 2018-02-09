daa718ec4bbaeb9b0fb3126008e589f5c8575a17
elecho1 <rikiauczatu@gmail.com>
2018-02-09 15:18:19 +0900

    fixed python2 example

--- examples/python/sample.py
+++ examples/python/sample.py
@@ -5,9 +5,9 @@ lib = cdll.LoadLibrary("../../libmomentum_url_normalizer.a")
 ptr = c_char_p("")
 lib.first_normalize_url("http://example.com/path/", pointer(ptr))
 print ptr.value
-lib.free_normalize_url(ptr)
+lib.free_normalize_url(pointer(ptr))
 
 ptr = c_char_p("")
 lib.second_normalize_url("http://example.com/path/", pointer(ptr))
 print ptr.value
-lib.free_normalize_url(ptr)
+lib.free_normalize_url(pointer(ptr))

99ad15afe3e35962875a280f0834665773369279
Takeshi Ito <takeshi.ito@m0mentum.co.jp>
2022-01-24 15:49:21 +0900

    fix(js): update packages to fix vulnerabilities

--- js/rollup.config.js
+++ js/rollup.config.js
@@ -2,8 +2,10 @@ import commonjs from 'rollup-plugin-commonjs';
 import nodeResolve from 'rollup-plugin-node-resolve';
 
 export default {
-  format: 'umd',
-  name: 'urlnorm',
+  output: {
+    format: 'umd',
+    name: 'urlnorm'
+  },
 
   plugins: [nodeResolve(), commonjs()]
 };
